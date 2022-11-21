a516666dbd50f8cb37e3be1e7d3481dc6a1a35c7
yuki-choki <ikuy0105@gmail.com>
2022-11-22 00:56:36 +0900

    バンドルツールをVITEからLaravel MIXに変更

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -19,6 +19,8 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
